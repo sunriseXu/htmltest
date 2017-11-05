52e96ac84245307bb26ec01b0f4d8d739c72a8cb
iMac <imac@iMac-02deiMac.local>
2017-11-05 17:20:12 +0800

    third

--- scripts/displayAbbreviations.js
+++ scripts/displayAbbreviations.js
@@ -1,1 +1,9 @@
- 
+function displayAbbreviations(){
+	var abbreviations=document.getElementsByTagName("abbr");
+	if(abbreviations.length<1) return false;
+	var defs=[];
+	for(var i =0; i<abbreviations.length; i++){
+		var definition=abbreviations[i].getAttribute("title");
+		
+	}
+}
